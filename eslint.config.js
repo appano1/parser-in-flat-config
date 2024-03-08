62887e273c3036c42b5de4f510c1e1134c259c0c
emoldino-sun <sun.lee@emoldino.com>
2024-03-08 15:45:41 +0900

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import vue from "eslint-plugin-vue";
+import vueParser from "vue-eslint-parser";
+import tsVue from "@vue/eslint-config-typescript";
+import * as tsParser from "typescript-eslint-parser-for-extra-files";
+import globals from "globals";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2017,
+      },
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+        parser: tsParser,
+      },
+    },
+  },
+  {
+    files: ["**/*.vue"],
+    plugins: { vue },
+    processor: vue.processors[".vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        ...tsVue.parserOptions,
+        parser: tsParser,
+      },
+    },
+    rules: {
+      ...vue.configs.base.rules,
+      ...vue.configs["vue3-essential"].rules,
+      ...vue.configs["vue3-strongly-recommended"].rules,
+      ...vue.configs["vue3-recommended"].rules,
+    },
+  },
+  {
+    files: tsVue.overrides[0].files.map((file) => `**/${file}`),
+    rules: {
+      ...tsVue.overrides[0].rules,
+    },
+  },
+);
